ffccab634efcff9c93770a46ef6ed391556e5f42
Julian Kamphausen <juliankamphausen@me.com>
2020-05-04 19:04:14 +0200

    some refactoring

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { getHTML, getTwitterFollowers, getInstagramFollowers } from './lib/scraper';
 
 async function main() {
-    const tCount = await getTwitterFollowers(await getHTML('https://twitter.com/jkamphausen93'));
-    const iCount = await getInstagramFollowers(await getHTML('https://instagram.com/jkamphausen'));
+    const tCount = await getTwitterFollowers('jkamphausen93');
+    const iCount = await getInstagramFollowers('jkamphausen');
 
     console.log(`You have ${tCount} followers on Twitter.`);
     console.log(`You have ${iCount} followers on Instagram.`);
