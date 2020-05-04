f85437e1abeae3fc102cbf3d35563e63fbf6e238
Julian Kamphausen <juliankamphausen@me.com>
2020-05-04 14:22:10 +0200

    scraping followers works!

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import { getHTML } from './lib/scraper';
+import { getHTML, getTwitterFollowers, getInstagramFollowers } from './lib/scraper';
 
-console.log(getHTML());
+async function main() {
+    const tCount = await getTwitterFollowers(await getHTML('https://twitter.com/jkamphausen93'));
+    const iCount = await getInstagramFollowers(await getHTML('https://instagram.com/jkamphausen'));
+
+    console.log(`You have ${tCount} followers on Twitter.`);
+    console.log(`You have ${iCount} followers on Instagram.`);
+}
+
+main();
