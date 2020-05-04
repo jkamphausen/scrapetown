551f6e3696662407a817affa999ea15d588da0be
Julian Kamphausen <juliankamphausen@me.com>
2020-05-04 23:21:29 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import express from 'express';
 import { getHTML, getTwitterFollowers, getInstagramFollowers } from './lib/scraper';
+import './lib/cron';
 
-async function main() {
+
+const app = express();
+
+app.get('/scrape', async (req, res, next) => {
+
+    console.info('🛢🥄 SCRAPING THE BARREL!'); //📝
 
     const [iCount, tCount] = await Promise.all([
         getTwitterFollowers('jkamphausen93'),
@@ -9,6 +16,8 @@ async function main() {
 
     console.log(`You have ${tCount} followers on Twitter.`);
     console.log(`You have ${iCount} followers on Instagram.`);
-}
+});
 
-main();
+const listener = app.listen(4211, () => {
+    console.info(`[Scrapetown] Running on http://localhost:${listener.address().port} 🥄`)
+})
